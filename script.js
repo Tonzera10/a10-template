e8fb100783b53be435d12b3d350b82c0d534e655
Everton Mello <everton.mello.281@gmail.com>
2022-07-27 21:26:17 -0300

    Exercícios feitos

---
+++ script.js
@@ -0,0 +1,13 @@
+const array = [3, 6, 2, 4, 8, 5];
+console.log(array);
+
+array.sort();
+console.log(array);
+
+array.reverse();
+console.log(array);
+
+array.pop();
+array.push(6);
+array.splice(2,1)
+console.log(array);
